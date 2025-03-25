e3afeb322968d2db95a0f62f41dca1c7201c6ad6
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-25 19:31:37 +0530

    Print Numbers 1 to 10

---
+++ src/com/Nandhakumar/LoopQuestions/PrintNumbers.java
@@ -0,0 +1,10 @@
+package com.Nandhakumar.LoopQuestions;
+
+public class PrintNumbers {
+    public static void main(String[] args){
+        // Print numbers 1 to 10
+        for (int i = 1; i <=10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/com/Nandhakumar/LoopQuestions/SumOfNumbers.java
@@ -0,0 +1,6 @@
+package com.Nandhakumar.LoopQuestions;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;i <=n; i++) {
