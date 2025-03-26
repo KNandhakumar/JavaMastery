895975c5b6dfec7ef55fc13628e3cfa4ce083ab2
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-26 11:28:34 +0530

    Sum of digits

--- src/com/Nandhakumar/WhileLoopQuestions/FindSumOfDigits.java
+++ src/com/Nandhakumar/WhileLoopQuestions/FindSumOfDigits.java
@@ -1,4 +1,22 @@
 package com.Nandhakumar.WhileLoopQuestions;
 
+import java.util.Scanner;
+
 public class FindSumOfDigits {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int remainder = 0;
+        int sum = 0;
+        if (n==0){
+            sum = 0;
+        }
+        n = Math.abs(n);
+        while (n>0){
+            remainder = n%10;
+            sum+=remainder;
+            n = n/10;
+        }
+        System.out.println(sum);
+    }
 }
