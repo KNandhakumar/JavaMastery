7c1dc0fff28bd650d4e2e864fefe5185f35e2b57
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-28 16:53:12 +0530

    Sum of two numbers using in method

---
+++ src/com/Nandhakumar/Methods/SumOfNumber.java
@@ -0,0 +1,13 @@
+package com.Nandhakumar.Methods;
+
+public class SumOfNumber {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 10;
+        int ans = sum(num1,num2);
+        System.out.println(ans);
+    }
+    static int sum(int n1,int n2){
+        return n1+n2;
+    }
+}
