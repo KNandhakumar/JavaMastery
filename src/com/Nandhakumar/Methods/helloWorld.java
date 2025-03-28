7ae3caf284a8ac2f8e539259583a0e2a357f96fa
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-28 16:55:54 +0530

    Hello world using method

---
+++ src/com/Nandhakumar/Methods/helloWorld.java
@@ -0,0 +1,11 @@
+package com.Nandhakumar.Methods;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        greeting();
+    }
+
+    static void greeting(){
+        System.out.println("Hello world");
+    }
+}
