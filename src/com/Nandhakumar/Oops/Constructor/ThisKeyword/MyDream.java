28f7cbf79b4516e1490e77ebef5c3a0680997a34
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-29 14:15:56 +0530

    This Keyword

---
+++ src/com/Nandhakumar/Oops/Constructor/ThisKeyword/MyDream.java
@@ -0,0 +1,23 @@
+package com.Nandhakumar.Oops.Constructor.ThisKeyword;
+
+public class MyDream {
+    String name;
+    int age;
+    String love;
+    // Default constructor
+    MyDream(String name,int age,String love){
+        this.name = name;
+        this.age = age;
+        this.love = love;
+    }
+    // creating method
+    void changeGoal(){
+        System.out.println("My Name :" + " "+name);
+        System.out.println("My Age :" + " "+age);
+        System.out.println("My Love :" + " "+love);
+    }
+    public static void main(String[] args) {
+        MyDream me = new MyDream("Nandhakumar",23,"Mami IAS");
+        me.changeGoal();
+    }
+}
