cb72f198c7fed5445b99a29063ca11dd0c574a3f
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-29 15:53:13 +0530

    Hierarchical inheritance

---
+++ src/com/Nandhakumar/Oops/Inheritance/Hierarchical/Main.java
@@ -0,0 +1,31 @@
+package com.Nandhakumar.Oops.Inheritance.Hierarchical;
+
+// super class
+class Animal{
+    void eat(){
+        System.out.println("this animal can eat");
+    }
+}
+
+// sub class
+class Dog{
+    void bark(){
+        System.out.println("it can bark");
+    }
+}
+
+// sub class
+class Pigeon{
+    void fly(){
+        System.out.println("it can fly");
+    }
+}
+
+
+// multiple subclasses inherit one super class that is hierarchical inheritance
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
