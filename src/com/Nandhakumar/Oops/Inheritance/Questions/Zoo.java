e7eba23a433a9ebb1257b419d14053660f96ccaf
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-29 18:03:31 +0530

    inheritance questions

---
+++ src/com/Nandhakumar/Oops/Inheritance/Questions/Zoo.java
@@ -0,0 +1,41 @@
+package com.Nandhakumar.Oops.Inheritance.Questions;
+// super class
+class Animal {
+    String name;
+    int age;
+    void makeSound(){
+        System.out.println("Animal makes a sound");
+    }
+}
+
+// sub class
+class Dog extends Animal{
+    String breed;
+    // override function
+    void makeSound(){
+        System.out.println("dog barks");
+    }
+    void fetch(){
+        System.out.println("dog is fetching");
+    }
+}
+
+// sub class
+class Husky extends Animal {
+    String color;
+    void makeSound(){
+        System.out.println("very sound barking");
+    }
+    void Cold(){
+        System.out.println("husky can survive low degree celsius");
+    }
+}
+
+public class Zoo {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+        Husky husky = new Husky();
+        husky.makeSound();
+    }
+}
