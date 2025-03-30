23f72ac5da7a8d129f1f0930e5f76ff9db902fdb
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-30 21:14:34 +0530

    final class

---
+++ src/com/Nandhakumar/Oops/Final/FinalClass.java
@@ -0,0 +1,17 @@
+package com.Nandhakumar.Oops.Final;
+
+// when i used final keyword front of class,
+final class Shape{
+
+}
+
+// final class did not accept to extents
+//class Diamond extends Shape{
+//
+//}
+
+public class FinalClass {
+    public static void main(String[] args) {
+
+    }
+}
