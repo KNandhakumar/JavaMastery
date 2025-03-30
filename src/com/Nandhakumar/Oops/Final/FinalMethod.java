3609b379c49aa7c89aea6462adeaabf1f1e1eef8
Nandhakumar <nknandhakumar64@gmail.com>
2025-03-30 21:08:01 +0530

    final method

---
+++ src/com/Nandhakumar/Oops/Final/FinalMethod.java
@@ -0,0 +1,19 @@
+package com.Nandhakumar.Oops.Final;
+
+class Animal {
+    final void display(){
+        System.out.println("this is final method for Animal class");
+    }
+}
+
+//class Dog extends Animal{
+//    void display(){
+//        System.out.println("cannot be override in final method");
+//    }
+//}
+
+public class FinalMethod {
+    public static void main(String[] args) {
+
+    }
+}
